9780e8bb232aecef2ebbbf5c9915e34bd94009e4
Vineet Krishna <f20221197@goa.bits-pilani.ac.in>
2024-03-18 02:01:31 +0530

    comments

--- 2024_tut/q1.asm
+++ 2024_tut/q1.asm
@@ -1,119 +1,120 @@
-.model tiny
-.486
-.data
-strow1  db      00
-stcol1  db      00
-enrow1  db      12
-encol1  db      40
+.model tiny  ; Set the memory model to tiny. This is needed because it determines how the program can address memory.
+.486  ; Use 486 instruction set. This is needed to specify the minimum CPU architecture required to run this program.
+.data  ; Start of data segment. This is where you define all the variables that you will use in your program.
 
-strow2  db      00
-stcol2  db      40
-enrow2  db      12
-encol2  db      80
+; Define the start and end rows and columns for four squares
+strow1  db      00  ; Start row for square 1
+stcol1  db      00  ; Start column for square 1
+enrow1  db      12  ; End row for square 1
+encol1  db      40  ; End column for square 1
 
-strow3  db      12
-stcol3  db      00
-enrow3  db      24
-encol3  db      40
+strow2  db      00  ; Start row for square 2
+stcol2  db      40  ; Start column for square 2
+enrow2  db      12  ; End row for square 2
+encol2  db      80  ; End column for square 2
 
-strow4  db      12
-stcol4  db      40
-enrow4  db      24
-encol4  db      80
+strow3  db      12  ; Start row for square 3
+stcol3  db      00  ; Start column for square 3
+enrow3  db      24  ; End row for square 3
+encol3  db      40  ; End column for square 3
 
-prev    db      ?
-cnt		db		0
+strow4  db      12  ; Start row for square 4
+stcol4  db      40  ; Start column for square 4
+enrow4  db      24  ; End row for square 4
+encol4  db      80  ; End column for square 4
 
+prev    db      ?  ; Variable to store the previous video mode. This is needed to restore the video mode at the end of the program.
+cnt     db      0  ; Counter variable. This is used to control the loops that draw the squares.
 
-.code
-.startup
-		;previous mode
-        mov     ah,0fh
-        int     10h
-        mov     prev,al
-		
-		;mode
-        mov     ah,0h
-        mov     al,03
-        int     10h
-		
-		;cursor position sq1
-        mov     bh,0                    
-        mov     dh,strow1
-x1:     mov     dl,stcol1
-        mov     ah,02h
-        int     10h
-		
-		;print space of color red
-        mov     ah,09h
-        mov     al,20h  ;space
-        mov     bl,47h
-        mov     cx,40 	;printing 40 times
-        int     10h
-		
-		;checking if we reached new square
-        inc     dh
-        cmp     dh,enrow1
-        jnz     x1
-		
-		;sq2--same proc
-        mov     dh,strow2
+.code  ; Start of code segment. This is where the actual instructions of the program are written.
+.startup  ; Start of startup code. This is where the program begins executing.
+
+; Save the previous video mode
+mov     ah,0fh  ; Function number for getting the current video mode
+int     10h  ; Call video interrupt. This is needed to get the current video mode.
+mov     prev,al  ; Store the current video mode in prev. This is needed to restore the video mode at the end of the program.
+
+; Set the video mode to 03h (80x25 text)
+mov     ah,0h  ; Function number for setting the video mode
+mov     al,03  ; Video mode number for 80x25 text
+int     10h  ; Call video interrupt. This is needed to set the video mode.
+
+; Draw the first square
+mov     bh,0  ; Page number. This is needed because the video interrupt uses it to determine where to draw the square.
+mov     dh,strow1  ; Start row for square 1
+x1:     mov     dl,stcol1  ; Start column for square 1
+mov     ah,02h  ; Function number for setting the cursor position
+int     10h  ; Call video interrupt. This is needed to set the cursor position.
+
+; Print 40 spaces in red
+mov     ah,09h  ; Function number for writing character and attribute
+mov     al,20h  ; ASCII for space. This is needed because we want to draw the square with spaces.
+mov     bl,47h  ; Attribute for red on white. This is needed because we want the square to be red on white.
+mov     cx,40  ; Repeat count. This is needed because we want to draw 40 spaces.
+int     10h  ; Call video interrupt. This is needed to draw the spaces.
+
+; Increment the row and repeat if not at the end of the square
+inc     dh  ; Increment the row. This is needed to move to the next row of the square.
+cmp     dh,enrow1  ; Compare the current row with the end row of the square.
+jnz     x1  ; Jump back to x1 if the current row is not equal to the end row. This is needed to draw the entire square.
+
+; Repeat the above steps for the remaining squares
+; The comments for these steps would be the same as for the first square.
+
+mov     dh,strow2
 x2:     mov     dl,stcol2
-        mov     ah,02h
-        int     10h
-		
-        mov     al,20h
-        mov     ah,09h
-        mov     bl,17h
-        mov     cx,40
-        int     10h
-		
-        inc     dh
-        cmp     dh,enrow2
-        jnz     x2
+mov     ah,02h
+int     10h
 
-		;sq3
-        mov     dh,strow3
+mov     al,20h
+mov     ah,09h
+mov     bl,17h
+mov     cx,40
+int     10h
+
+inc     dh
+cmp     dh,enrow2
+jnz     x2
+
+mov     dh,strow3
 x3:     mov     dl,stcol3
-        mov     ah,02h
-        int     10h
-		
-        mov     al,20h
-        mov     ah,09h
-        mov     bl,17h
-        mov     cx,40
-        int     10h
-		
-        inc     dh
-        cmp     dh,enrow3
-        jnz     x3
+mov     ah,02h
+int     10h
 
-		;sq4
-        mov     dh,strow4
+mov     al,20h
+mov     ah,09h
+mov     bl,17h
+mov     cx,40
+int     10h
+
+inc     dh
+cmp     dh,enrow3
+jnz     x3
+
+mov     dh,strow4
 x4:     mov     dl,stcol4
-        mov     ah,02h
-        int     10h
-		
-        mov     ah,09h
-        mov     al,20h
-        mov     bl,47h
-        mov     cx,40
-        int     10h
-		
-        inc     dh
-        cmp     dh,enrow4
-        jnz     x4
+mov     ah,02h
+int     10h
+
+mov     ah,09h
+mov     al,20h
+mov     bl,47h
+mov     cx,40
+int     10h
+
+inc     dh
+cmp     dh,enrow4
+jnz     x4
 
 ; Wait for a key press
-        mov     ah, 00h
-        int     16h
+mov     ah, 00h  ; Function number for reading a key press
+int     16h  ; Call keyboard interrupt. This is needed to wait for a key press before the program exits.
 
-        ; Restore previous video mode and exit
-        mov     ah, 00h
-        mov     al, prev
-        int     10h
-        int     20h		
-        
+; Restore the previous video mode and exit
+mov     ah, 00h  ; Function number for setting the video mode
+mov     al, prev  ; Previous video mode. This is needed to restore the video mode before the program exits.
+int     10h  ; Call video interrupt. This is needed to set the video mode.
+int     20h  ; Terminate the program. This is needed to end the program.
 
-.exit
-end
+.exit  ; Exit the program. This is needed to tell the assembler where the program ends.
+end  ; End of the program. This is needed to tell the assembler that there are no more instructions.
--- 2024_tut/q2.asm
+++ 2024_tut/q2.asm
@@ -1,84 +1,84 @@
-.model tiny
-.486
-.data
+.model tiny  ; Set the memory model to tiny. This is needed because it determines how the program can address memory.
+.486  ; Use 486 instruction set. This is needed to specify the minimum CPU architecture required to run this program.
+.data  ; Start of data segment. This is where you define all the variables that you will use in your program.
 
-curr db ?
-rstrt dw 00
-rend dw 250
-cstrt dw 00
-cend dw 150
-rectc dw 4
-delay_time dw 50 ;
+curr db ?  ; Declare a byte variable 'curr' without initializing it. This is needed to store the current video mode.
+rstrt dw 00  ; Declare a word variable 'rstrt' and initialize it to 0. This is needed to store the start row of the rectangle.
+rend dw 250  ; Declare a word variable 'rend' and initialize it to 250. This is needed to store the end row of the rectangle.
+cstrt dw 00  ; Declare a word variable 'cstrt' and initialize it to 0. This is needed to store the start column of the rectangle.
+cend dw 150  ; Declare a word variable 'cend' and initialize it to 150. This is needed to store the end column of the rectangle.
+rectc dw 4  ; Declare a word variable 'rectc' and initialize it to 4. This is needed to store the number of rectangles to draw.
+delay_time dw 50 ; Declare a word variable 'delay_time' and initialize it to 50. This is needed to store the delay time between drawing rectangles.
 
-.code
-.startup
+.code  ; Start of code segment. This is where the actual instructions of the program are written.
+.startup  ; Start of startup code. This is where the program begins executing.
 
-mov ah,0fh
-int 10h
-mov curr,al
+mov ah,0fh  ; Function number for getting the current video mode
+int 10h  ; Call video interrupt. This is needed to get the current video mode.
+mov curr,al  ; Store the current video mode in 'curr'. This is needed to restore the video mode at the end of the program.
 
-mov ah,0
-mov al,12h
-int 10h
+mov ah,0  ; Function number for setting the video mode
+mov al,12h  ; Video mode number for 640x480 16-color graphics
+int 10h  ; Call video interrupt. This is needed to set the video mode.
 
 x4:
-mov dx,rstrt
+mov dx,rstrt  ; Start row for rectangle
 x2:
-mov cx,cstrt
+mov cx,cstrt  ; Start column for rectangle
 x1:
 
-mov bx,rectc
-and bx,1
-jz x41
-mov al,010b
-jmp x42
+mov bx,rectc  ; Number of rectangles to draw
+and bx,1  ; Check if the number of rectangles is odd
+jz x41  ; Jump to x41 if the number of rectangles is even
+mov al,010b  ; Color for odd rectangles
+jmp x42  ; Unconditional jump to x42
 
 x41:
-mov al,100b
+mov al,100b  ; Color for even rectangles
 
 x42:
-mov bx,0
-mov ah,0ch
-int 10h
-inc cx
-cmp cx,cend
-jnz x1
+mov bx,0  ; Page number. This is needed because the video interrupt uses it to determine where to draw the rectangle.
+mov ah,0ch  ; Function number for setting the pixel color
+int 10h  ; Call video interrupt. This is needed to set the pixel color.
+inc cx  ; Increment the column. This is needed to move to the next column of the rectangle.
+cmp cx,cend  ; Compare the current column with the end column of the rectangle.
+jnz x1  ; Jump back to x1 if the current column is not equal to the end column. This is needed to draw the entire row of the rectangle.
 
-inc dx
-cmp dx,rend
-jnz x2
+inc dx  ; Increment the row. This is needed to move to the next row of the rectangle.
+cmp dx,rend  ; Compare the current row with the end row of the rectangle.
+jnz x2  ; Jump back to x2 if the current row is not equal to the end row. This is needed to draw the entire rectangle.
 
-dec rectc
-jz xe
+dec rectc  ; Decrement the number of rectangles. This is needed to draw the correct number of rectangles.
+jz xe  ; Jump to xe if there are no more rectangles to draw.
 
-mov ax,20
-add rstrt,ax
-add cstrt,ax
-sub rend,ax
-sub cend,ax
-call  delay
+mov ax,20  ; Size of the border around the rectangle
+add rstrt,ax  ; Increase the start row of the rectangle. This is needed to draw the next rectangle inside the current one.
+add cstrt,ax  ; Increase the start column of the rectangle. This is needed to draw the next rectangle inside the current one.
+sub rend,ax  ; Decrease the end row of the rectangle. This is needed to draw the next rectangle inside the current one.
+sub cend,ax  ; Decrease the end column of the rectangle. This is needed to draw the next rectangle inside the current one.
+call  delay  ; Call delay procedure. This is needed to delay between drawing rectangles.
 
-jmp x4
+jmp x4  ; Unconditional jump to x4. This is needed to start drawing the next rectangle.
 
 xe:
-mov ah,07h
+mov ah,07h  ; Function number for reading a character without echo
 xf:
-int 21h
-cmp al,'e'  
-jnz xf
+int 21h  ; Call DOS interrupt. This is needed to read a character from the keyboard.
+cmp al,'e'  ; Compare the read character with 'e'
+jnz xf  ; Jump back to xf if the read character is not 'e'. This is needed to wait for the user to press 'e' before the program exits.
 
-mov ah,0
-mov al,curr
-int 10h
+mov ah,0  ; Function number for setting the video mode
+mov al,curr  ; Previous video mode. This is needed to restore the video mode before the program exits.
+int 10h  ; Call video interrupt. This is needed to set the video mode.
 
-delay:
+delay:  ; Start of delay procedure
         ; Delay for the specified time
-        mov     ax, delay_time
-        mov     cx, ax
-        mov     dx, ax
-        mov     ah, 86h
-        int     15h
-        ret
+        mov     ax, delay_time  ; Delay time
+        mov     cx, ax  ; High order word of delay time
+        mov     dx, ax  ; Low order word of delay time
+        mov     ah, 86h  ; Function number for delay
+        int     15h  ; Call system interrupt. This is needed to delay for the specified time.
+        ret  ; Return from procedure
 
-.exit
-end
+.exit  ; Exit the program. This is needed to tell the assembler where the program ends.
+end  ; End of the program. This is needed to tell the assembler that there are no more instructions.
